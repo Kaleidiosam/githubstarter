db7ed05e4942d8764a060d8d8e1a141b0df18a5b
Kaleidiosam <139863023+Kaleidiosam@users.noreply.github.com>
2023-07-18 12:05:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,24 @@
 // Create web swerver
-// Load HTTP module
-var http = require("http");
-// File system
-var fs = require("fs");
-// Create server
-http.createServer(function(request, response) {
-	// Send the HTTP header
-	// HTTP Status: 200 : OK
-	// Content Type: text/plain
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	// Send the response body as "Hello World"
-	response.end("Hello World\n");
-}).listen(8001);
-// Console will print the message
-console.log("Server running at http://
+// 1. npm init
+// 2. npm install express
+// 3. node comments.js
+// 4. http://localhost:3000/comments
+// 5. http://localhost:3000/comments/1
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// http://localhost:3000/comments
+app.get('/comments', (req, res) => {
+    res.send('Comments')
+})
+
+// http://localhost:3000/comments/1
+app.get('/comments/:id', (req, res) => {
+    res.send('Comments ' + req.params.id)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
